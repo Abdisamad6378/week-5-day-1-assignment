4a455538c12a3b39bf74668a306af2b82ac7f7e7
Abdisamad6378 <abdisamadhussein6378@gmail.com>
2026-08-01 23:32:51 +0300

    Set up Express server with Kenyan cities data

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let cities = [
+  { id: 1, name: "Nairobi", county: "Nairobi", population: 4397073 },
+  { id: 2, name: "Mombasa", county: "Mombasa", population: 1208333 },
+  { id: 3, name: "Kisumu", county: "Kisumu", population: 610082 },
+  { id: 4, name: "Nakuru", county: "Nakuru", population: 570674 },
+  { id: 5, name: "Eldoret", county: "Uasin Gishu", population: 475716 },
+  { id: 6, name: "Thika", county: "Kiambu", population: 279429 },
+  { id: 7, name: "Malindi", county: "Kilifi", population: 207253 },
+  { id: 8, name: "Kitale", county: "Trans-Nzoia", population: 220111 }
+];
+
+app.get("/api/cities", (req, res) => {
+  res.status(200).json({ success: true, count: cities.length, data: cities });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
